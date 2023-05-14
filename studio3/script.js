9a32a74594bf40592141dd1ce4143f8ed34244ba
Kaelyn Syrdahl <kasyrdahl@ucdavis.edu>
2023-05-14 16:24:49 -0700

    interviews

--- studio3/script.js
+++ studio3/script.js
@@ -2,12 +2,22 @@
     'use strict';
     console.log('reading js');
 
-    document.querySelector('#ca2').addEventListener('mouseover', function(){
+    document.querySelector('#ca2').addEventListener('mouseover', showstate);
+    
+    function showstate(){
         document.querySelector('#ca').className ='showing';
-    })
+        document.querySelector('#ca').addEventListener('mouseover', function(){
+            document.querySelector('#ca2').removeEventListener('mouseout', hidestate);   
+        })
+    }
 
-    document.querySelector('#ca2').addEventListener('mouseout', function(){
+    document.querySelector('#ca2').addEventListener('mouseout', hidestate);
+
+    function hidestate(){
         document.querySelector('#ca').className ='hidden';
-    })
+        document.querySelector('#ca').addEventListener('mouseout', function(){
+            document.querySelector('#ca2').removeEventListener('mouseover', showstate);   
+        })
+    }
 
 })()
