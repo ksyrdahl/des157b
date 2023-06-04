854a3af6fcb059830b9157fbb80aa1cae5c39343
Kaelyn Syrdahl <kasyrdahl@ucdavis.edu>
2023-06-03 17:55:36 -0700

    sec

--- version3/script.js
+++ version3/script.js
@@ -8,6 +8,7 @@
     let map = document.getElementById("bricks");
     let mapScale = 1;
     const mapScaleValue = .2;
+    const mapScaleSec = .4;
 // Code for searching
     const cards = document.querySelectorAll('.box')
 //A little delay for the search
@@ -22,7 +23,7 @@
     const triggers = document.querySelectorAll('.trigger');
     const brickSection = document.getElementById('brickSection');
     const letterBricks = document.getElementById('bricktrig8');
-    let zoomed = '2';
+    const bricks = document.getElementById('bricks');
 // This code highlights the bricks
     const individuals = document.querySelectorAll('.individual');
     const dresbach = document.getElementById('dresbach');
@@ -114,26 +115,132 @@
 
     triggers.forEach(function(trigger){
         trigger.addEventListener('click', function(event){
-            console.log(event.target.id);
+            const num = event.target.id;
+            console.log(num);
             brickSection.innerHTML = `<img src="images/${event.target.id}.svg">`;
             console.log(brickSection.innerHTML);
             brickSection.className = 'is-showing';
-            console.log(brickSection.className);
+            // console.log(brickSection.className);
             const bsection = document.getElementById(`sec${event.target.id}`);
             bsection.className = 'is-hidden';
             letterBricks.className = 'is-showing';
+            if(num === "trig1") {
+                console.log('u pressed a');
+                bricks.style.top = '55%';
+                bricks.style.left = '40%';
+                mapScale += 1.5;
+                map.style.transform = `scale(${mapScale}`;
+            }else if(num === "trig2") {
+                console.log('u pressed b');
+                mapScale += 2;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '30%';
+                bricks.style.left = '80%';
+            } else if(num === "trig3") {
+                console.log('u pressed c');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '20%';
+                bricks.style.left = '90%';
+            } else if(num === "trig4") {
+                console.log('u pressed d');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '-20%';
+                bricks.style.left = '80%';
+            } else if(num === "trig5") {
+                console.log('u pressed e');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '55%';
+                bricks.style.left = '90%';
+            } else if(num === "trig6") {
+                console.log('u pressed f');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '55%';
+                bricks.style.left = '90%';
+            } else if(num === "trig7") {
+                console.log('u pressed g');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '55%';
+                bricks.style.left = '90%';
+            } else if(num === "trigg8") {
+                console.log('u pressed h');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '-40%';
+                bricks.style.left = '50%';
+            } else {
+                console.log('bye');
+            }
         })
         trigger.addEventListener('touchstart', function(event){
-            console.log(event.target.id);
-            brickSection.innerHTML = `<img src="images/${event.target.id}.svg">`;
+            // console.log(event.target.id);
+            brickSection.innerHTML = `<img src="images/${event.target.id}.svg" id="single${event.target.id}">`;
             console.log(brickSection.innerHTML);
             brickSection.className = 'is-showing';
-            console.log(brickSection.className);
+            // console.log(brickSection.className);
             const bsection = document.getElementById(`sec${event.target.id}`);
             bsection.className = 'is-hidden';
             letterBricks.className = 'is-showing';
+            mapScale += mapScaleSec;
+            map.style.transform = `scale(${mapScale}`;
+            if(num === "trig1") {
+                console.log('u pressed a');
+                bricks.style.top = '55%';
+                bricks.style.left = '40%';
+                mapScale += 1.5;
+                map.style.transform = `scale(${mapScale}`;
+            }else if(num === "trig2") {
+                console.log('u pressed b');
+                mapScale += 2;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '30%';
+                bricks.style.left = '80%';
+            } else if(num === "trig3") {
+                console.log('u pressed c');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '20%';
+                bricks.style.left = '90%';
+            } else if(num === "trig4") {
+                console.log('u pressed d');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '-20%';
+                bricks.style.left = '80%';
+            } else if(num === "trig5") {
+                console.log('u pressed e');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '55%';
+                bricks.style.left = '90%';
+            } else if(num === "trig6") {
+                console.log('u pressed f');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '55%';
+                bricks.style.left = '90%';
+            } else if(num === "trig7") {
+                console.log('u pressed g');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '55%';
+                bricks.style.left = '90%';
+            } else if(num === "trigg8") {
+                console.log('u pressed h');
+                mapScale += 1.8;
+                map.style.transform = `scale(${mapScale}`;
+                bricks.style.top = '-40%';
+                bricks.style.left = '50%';
+            } else {
+                console.log('bye');
+            }
         })
     })
+
 
 // This code highlights the bricks
 
